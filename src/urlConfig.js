a6283ad28625e4ae1cdbd9ba7ee6ab08a09625d5
Naveen Verma <nveenverma@gmail.com>
2021-06-30 04:16:49 +0400

    Change URL format to display images from AWS

--- src/urlConfig.js
+++ src/urlConfig.js
@@ -3,5 +3,5 @@ const baseUrl = "https://backend-srvr-ecommerce.herokuapp.com"
 export const api = `${baseUrl}/api`;
 
 export const generatePublicUrl = (fileName) => {
-    return `${baseUrl}/public/${fileName}`
+    return `fileName`
 }
