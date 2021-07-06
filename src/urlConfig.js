9625d086ed74f6d77385fbed6a7f01e1493bfbca
Naveen Verma <nveenverma@gmail.com>
2021-07-06 10:21:42 +0400

    Updated urlConfig withAbaseUrl from AWS

--- src/urlConfig.js
+++ src/urlConfig.js
@@ -1,2 +1,2 @@
-const baseUrl = "https://backend-srvr-ecommerce.herokuapp.com"
+const baseUrl = "http://backendsrvrecommerce-env.eba-q2ebaymr.ap-south-1.elasticbeanstalk.com"
 export const api = `${baseUrl}/api`;
