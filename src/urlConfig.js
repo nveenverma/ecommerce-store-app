c3232cf05061c1c6f6a49bd527345d304f9feb2a
Naveen Verma <nveenverma@gmail.com>
2021-07-07 11:20:22 +0400

    Updated urlConfig with baseUrl from Heroku

--- src/urlConfig.js
+++ src/urlConfig.js
@@ -1,2 +1,2 @@
-const baseUrl = "http://backendsrvrecommerce-env.eba-q2ebaymr.ap-south-1.elasticbeanstalk.com"
+const baseUrl = "https://backend-srvr-ecommerce.herokuapp.com"
 export const api = `${baseUrl}/api`;
